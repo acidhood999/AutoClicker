3661182e5b8fc1ae288302541aa17910d3d4b0fe
acidhood999 <nazar.kondrashov616@gmail.com>
2026-06-28 22:41:49 +0300

    v2

--- Functional.cpp
+++ Functional.cpp
@@ -4,36 +4,89 @@ Functional::Functional(QWidget* parent) : QWidget(parent)
 {
     toolsContainer = new QGridLayout(this);
 
-    QGroupBox* resolutionGroup = new QGroupBox("Resolution", this);
-    QGridLayout* resolutionLayout = new QGridLayout(resolutionGroup);
+    QGroupBox* clickIntervalGroup = new QGroupBox("Click Interval", this);
+    QGridLayout* clickIntervalLayout = new QGridLayout(clickIntervalGroup);
 
     initializationInterval(lines);
 
-    resolutionLayout->addWidget(lines[0], 0, 0);
-    resolutionLayout->addWidget(new QLabel("h", this), 0, 1);
-    resolutionLayout->addWidget(lines[1], 0, 2);
-    resolutionLayout->addWidget(new QLabel("m", this), 0, 3);
-    resolutionLayout->addWidget(lines[2], 0, 4);
-    resolutionLayout->addWidget(new QLabel("s", this), 0, 5);
-    resolutionLayout->addWidget(lines[3], 0, 6);
-    resolutionLayout->addWidget(new QLabel("ms", this), 0, 7);
-
-    toolsContainer->addWidget(resolutionGroup, 0, 0);
+    clickIntervalLayout->addWidget(lines[0], 0, 0);
+    clickIntervalLayout->addWidget(new QLabel("h", this), 0, 1);
+    clickIntervalLayout->addWidget(lines[1], 0, 2);
+    clickIntervalLayout->addWidget(new QLabel("m", this), 0, 3);
+    clickIntervalLayout->addWidget(lines[2], 0, 4);
+    clickIntervalLayout->addWidget(new QLabel("s", this), 0, 5);
+    clickIntervalLayout->addWidget(lines[3], 0, 6);
+    clickIntervalLayout->addWidget(new QLabel("ms", this), 0, 7);
 
 
+    QGroupBox* buttonsGroup = new QGroupBox(this);
+    QGridLayout* buttonsLayout = new QGridLayout(buttonsGroup);
+
+    initializationButtons(buttons);
+    buttonsLayout->addWidget(buttons[0], 1, 0);
+    buttonsLayout->addWidget(buttons[1],1, 1);
+
+
+
+
+
+
+
+
+
+    toolsContainer->addWidget(clickIntervalGroup, 0, 0);
+    toolsContainer->addWidget(buttonsGroup, 1, 0);
+
+    connect(buttons[0], &QPushButton::clicked, this, &Functional::buttonsClickStart);
+    connect(buttons[1], &QPushButton::clicked, this, &Functional::buttonsClickStop);
 
 }
 
 void Functional::initializationInterval(QVector<QPointer<QLineEdit>>& lines)
 {
+ 
     lines.resize(4);
     for (int i = 0; i < lines.size();++i)
     {
         lines[i] = new QLineEdit(this);
-
+        lines[i]->setFocusPolicy(Qt::StrongFocus);
+        lines[i]->installEventFilter(this);
+        lines[i]->setAlignment(Qt::AlignRight);
+        lines[i]->setContextMenuPolicy(Qt::NoContextMenu);
+        lines[i]->setValidator(new QIntValidator(this));
     }
 }
 
+void Functional::initializationButtons(QVector<QPointer<QPushButton>>& buttons)
+{
+    buttons.resize(2);
+    for (int i = 0; i < buttons.size();++i) buttons[i] = new QPushButton(this);
+
+    buttons[0]->setText("Start");
+    buttons[1]->setText("Stop");
+
+
+}
+void Functional::buttonsClickStart()
+{
+    runCur = true;
+        QtConcurrent::run([this]()
+        {
+                while (runCur)
+                {
+                    ClickLMB();
+                }
+        });
+    
+    
+
+    
+}
+void Functional::buttonsClickStop()
+{
+    runCur = false;
+   
+}
 Functional::~Functional()
 {
 }
--- Functional.h
+++ Functional.h
@@ -8,17 +8,44 @@
 #include <QLabel>
 #include <QLineEdit>
 #include <QVector>
+#include <QIntValidator>
+#include <windows.h>								
+#include <stdio.h>	 
+#include <QtConcurrent>
+#include <atomic>
 
 class Functional : public QWidget
 {
 	Q_OBJECT
 public:
 	Functional(QWidget* parent);
-	~Functional();
+	~Functional();	
+private slots:
+	void buttonsClickStart();
+	void buttonsClickStop();
 private:
 	QPointer<QGridLayout> toolsContainer;
-	QPointer<QPushButton> b = new QPushButton("ewr");
 	QVector<QPointer<QLineEdit>> lines;
+	QVector<QPointer<QPushButton>> buttons;
+	std::atomic<bool> runCur{ false };
 
 	void initializationInterval(QVector<QPointer<QLineEdit>>& lines);
+	void initializationButtons(QVector<QPointer<QPushButton>>& buttons);
+
+	void ClickLMB()										
+	{
+		INPUT input;									
+
+		input.type = 0;									
+		input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;							
+		input.mi.mouseData = 0;							
+		input.mi.time = 0;								
+
+		SendInput(1, &input, sizeof(INPUT));			
+		input.mi.dwFlags = 4;							
+		Sleep(100);										
+
+		SendInput(1, &input, sizeof(INPUT));			
+	}
+
 };
